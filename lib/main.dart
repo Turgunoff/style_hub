dba3779ed1ef183a4e678bc990ca694b47074ab9
Eldor <eldor.turgunov.94@yandex.ru>
2025-02-06 23:59:07 -0800

    new

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,6 @@ class MyApp extends StatelessWidget {
       // Ilova nomi
       title: AppConstants.appName,
 
-      // Debug banner ni o'chirish
       debugShowCheckedModeBanner: false,
 
       // Tema sozlamalari
