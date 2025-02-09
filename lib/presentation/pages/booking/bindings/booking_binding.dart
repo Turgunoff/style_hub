ef7ccc43971cfc37ccc15818930a85e376cda812
Eldor <eldor.turgunov.94@yandex.ru>
2025-02-09 00:11:29 -0800

    new

--- lib/presentation/pages/booking/bindings/booking_binding.dart
+++ lib/presentation/pages/booking/bindings/booking_binding.dart
@@ -2,13 +2,9 @@ import 'package:get/get.dart';
 
 import '../../../controllers/booking_controller.dart';
 
-// import '../../../controllers/booking_controller.dart';
-
 class BookingBinding extends Bindings {
   @override
   void dependencies() {
     Get.put(BookingController());
-
-    // Add controllers if needed
   }
 }
